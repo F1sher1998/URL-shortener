e0306d37acd88def93db74ae901f15e03510cba8
F1sher1998 <ivanprotsyk1998@gmail.com>
2025-09-23 17:00:10 +0200

    updated user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,10 @@
-import { uuid, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { uuid, integer, pgTable, varchar, text, timestamp } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
